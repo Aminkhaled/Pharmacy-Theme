cd8ad8d151904cc9a31438f9dfd6a9741f31ebdf
alsun <aminabdo8@gmail.com>
2018-02-02 08:48:28 +0200

    finish products silder and most of it and third man section

--- js/plugin.js
+++ js/plugin.js
@@ -8,5 +8,32 @@ $(document).ready(function () {
             pauseOnHover: true
         }
     });
+    $("#flexiselDemo4").flexisel({
+        visibleItems: 3,
+        itemsToScroll: 1,
+        autoPlay: {
+            enable: true,
+            interval: 5000,
+            pauseOnHover: true
+        }
+    });
+    $("#flexiselDemo5").flexisel({
+        visibleItems: 3,
+        itemsToScroll: 1,
+        autoPlay: {
+            enable: true,
+            interval: 5000,
+            pauseOnHover: true
+        }
+    });
+    $("#flexiselDemo6").flexisel({
+        visibleItems: 3,
+        itemsToScroll: 1,
+        autoPlay: {
+            enable: true,
+            interval: 5000,
+            pauseOnHover: true
+        }
+    });
 
 });
